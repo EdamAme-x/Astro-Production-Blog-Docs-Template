a33c4e2f6003bdda025ca76cc82ca14fa497f940
EdamAme-x <edame8080@gmail.com>
2023-10-21 11:54:28 +0900

    go

--- src/src/consts.ts
+++ src/src/consts.ts
@@ -1,5 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Super Site';
+export const SITE_DESCRIPTION = 'Welcome to my Super Site';
+export const SITE_ICON = '/favicon.svg'; // □
+export const VIEW_BUTTON  = 'Get Started';
+export const AUTHOR_NAME = '@amex2189'; // your name
+export const SNS = {
+    TWITTER: "amex2189",
+    GITHUB: "EdamAme-x"
+}
